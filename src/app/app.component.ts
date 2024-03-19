f82103521dae76fb030ac8d16ec64de56cfe7ece
jota santos <gestald118@email.com>
2024-03-19 08:26:10 -0400

    Entendendo o componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, CommonModule], 
+  template: `
+    <!-- <router-outlet /> -->
+    <h1>Curso de Angular</h1>
+    <h2>Components</h2>
+
+    {{ title }}
+  `  
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
 }
+
+
