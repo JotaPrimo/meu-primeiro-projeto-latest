37aa9e6ddf7fbac513959f6cdc3f3d8b90e21fda
Jota Santos <jailsonsantosdpge@outlook.com>
2024-03-19 14:36:20 -0400

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,16 @@ import { RouterOutlet } from '@angular/router';
 // components
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent], 
+  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent], 
   template: `
     <!-- <router-outlet /> -->
     <h1>Curso de Angular</h1>
-    <app-template-binding />    
+    <app-template-variables />    
    
   `  
 })
