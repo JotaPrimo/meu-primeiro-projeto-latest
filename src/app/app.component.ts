e9a77aeb0e680a3443f783b6bf8a3679fe5c4c5a
jota santos <gestald118@email.com>
2024-03-19 08:33:10 -0400

    Criando um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// components
+import { NewComponent } from './components/new-component/new-component.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule], 
+  imports: [RouterOutlet, CommonModule, NewComponent], 
   template: `
     <!-- <router-outlet /> -->
-    <h1>Curso de Angular</h1>
-    <h2>Components</h2>
-
+    <h1>Curso de Angular</h1>    
+    <app-new-component />
     {{ title }}
   `  
 })
