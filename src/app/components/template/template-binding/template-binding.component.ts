9563d3def995304dae5ab0f23d2222a037dee087
Jota Santos <jailsonsantosdpge@outlook.com>
2024-03-19 14:21:00 -0400

    Two-way binding

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
 export class TemplateBindingComponent {
   public nome: String = "Jota Santos";
-  public age: Number = 29;
+  public age: number = 29;
   public isDisabled: Boolean = true;
 
   // image
   public srcImage: String = "https://avatars.githubusercontent.com/u/63259982?v=4";
   public altImage: String = "Fot do github";
   public titleImage: String = "Alt da image";
+
+  public sum() {
+    this.age++;
+  }
+
+  public sub() {
+    this.age--;
+  }
 }
