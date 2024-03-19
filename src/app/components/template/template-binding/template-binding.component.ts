7846af4a479c1e0975faf14af0bcc22269c434ed
Jota Santos <jailsonsantosdpge@outlook.com>
2024-03-19 14:05:17 -0400

    Property binding

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -11,6 +11,10 @@ import { CommonModule } from '@angular/common';
 export class TemplateBindingComponent {
   public nome: String = "Jota Santos";
   public age: Number = 29;
+  public isDisabled: Boolean = true;
 
-
+  // image
+  public srcImage: String = "https://avatars.githubusercontent.com/u/63259982?v=4";
+  public altImage: String = "Fot do github";
+  public titleImage: String = "Alt da image";
 }
